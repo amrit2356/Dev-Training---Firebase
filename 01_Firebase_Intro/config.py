4d927a0677cc3daba0adaaa07bc46ee1344bda06
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2021-01-14 19:27:28 +0530

    Firebase App Config

---
+++ 01_Firebase_Intro/config.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+    'apiKey': "AIzaSyCZMSrVMWwKT7iCr3dhcE0Fe5q9u8XEzIk",
+    'authDomain': "fir-course-184ac.firebaseapp.com",
+    'databaseURL': "https://fir-course-184ac-default-rtdb.firebaseio.com",
+    'projectId': "fir-course-184ac",
+    'storageBucket': "fir-course-184ac.appspot.com",
+    'messagingSenderId': "278554663344",
+    'appId': "1:278554663344:web:5f55da3f455ef810fe1e75",
+    'measurementId': "G-J4CM6V3MPD"
+  }
