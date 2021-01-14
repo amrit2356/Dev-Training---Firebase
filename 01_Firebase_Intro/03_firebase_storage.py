91bd138b12673c78388758cdb663ee69e94cb0f1
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2021-01-14 20:00:51 +0530

    Uploading file to Firebase Storage

---
+++ 01_Firebase_Intro/03_firebase_storage.py
@@ -0,0 +1,29 @@
+import pyrebase
+from config import firebaseConfig
+
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+# Authentication
+# Login
+try:
+    email = input("Enter Your Email ")
+    password = input("Enter Your Password ")
+    auth.sign_in_with_email_and_password(email,password)
+
+except:
+    print("Invalid Email or Password.")
+
+else:
+    print("Successfully Signed In")
+    # Storage
+    storage = firebase.storage()
+    filename = input("Enter the filename you want to import.")
+    cloudfilename = input("Enter the filename you want to have in cloud")
+    try:
+        # Uploading a sample text file on Firebase.
+        storage.child(cloudfilename).put(filename)
+    except:
+        print("Permission Denied...Check the permissions on firebase account")
+    else:
+        print("File Uploaded...")
