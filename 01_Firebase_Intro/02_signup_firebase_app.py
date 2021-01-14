c0a64c6361bd37a5cba86aa7cef533d09cbcdd09
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2021-01-14 19:29:26 +0530

    signup to new firebase app

--- 01_Firebase_Intro/02_signup_firebase_app.py
+++ 01_Firebase_Intro/02_signup_firebase_app.py
@@ -6,16 +6,16 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 
 #Signup Account
-try:
+
     new_email = input("Enter your email")
     new_password = input('Enter your Password')
     confirm_password = input('Reenter your Password to confirm')
-    if new_password == confirm_password:
-        auth.create_user_with_email_and_password(new_email,new_password)
+    try:
+        if new_password == confirm_password:
+            auth.create_user_with_email_and_password(new_email,new_password)
+    except:
+        print("Passwords not entered properly.")
 
-except:
-    print("Passwords not entered properly.")
-
-else:
-    print("Sign in Complete. Welcome to my First App on Firebase :P.")
+    else:
+        print("Sign in Complete. Welcome to my First App on Firebase :P.")
 
